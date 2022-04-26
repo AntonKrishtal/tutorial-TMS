3b52e5b70383c8616711cc8cb19a221e7542fa26
AntonKrishtal <104370345+AntonKrishtal@users.noreply.github.com>
2022-04-26 15:04:31 +0300

    commit homework

--- dz_7.py
+++ dz_7.py
@@ -1,2 +1,22 @@
-vdgdf
-s
+
+num_dict = {}
+def search(num):
+    for i in num:
+        if num_dict.get(i):
+            num_dict[i] += 1
+        else: 
+            num_dict[i] = 1
+    print(num_dict)
+
+search(num = [1, 2, 3, 4, 1, 2, 3, 1, 2, 1])
+
+# num_list = [1, 2, 3, 4, 1, 2, 3, 1, 2, 1]
+# print(num_list.count(1))
+# print(num_list.count(2))
+# print(num_list.count(3))
+# print(num_list.count(4))
+
+
+
+
+
